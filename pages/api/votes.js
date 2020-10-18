3cca31bd1ac547c5a25b77c03d011c43c36fed0a
bhiller <benhiller@gmail.com>
2020-10-18 13:34:21 -0700

    return election results from api endpoint

--- pages/api/votes.js
+++ pages/api/votes.js
@@ -1,7 +1,17 @@
 import knex from '../../knex';
 
 export default async function handler(req, res) {
-  const candidates = await knex('candidate').select('id', 'name');
+  const candidates = await knex('candidate')
+    .select({
+      candidate_id: 'candidate.id',
+      candidate_name: 'candidate.name',
+      contest_id: 'candidate.contest_id',
+      contest_name: 'contest.name',
+    })
+    .count({ votes: 'vote.id' })
+    .join('contest', 'contest.id', '=', 'candidate.contest_id')
+    .join('vote', 'vote.candidate_id', '=', 'candidate.id')
+    .groupBy('candidate.id');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(candidates));
