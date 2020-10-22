7f6a134fdc1039b6ad11b406accd62fec7436280
bhiller <benhiller@gmail.com>
2020-10-21 23:46:24 -0700

    remove sqlite env

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3',
-    },
-    useNullAsDefault: true,
-  },
-
-  psql: {
     client: 'postgresql',
     connection: process.env.PG_CONNECTION_STRING,
   },
