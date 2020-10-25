75c34c810bade8e27d9c592ccacd8826e6bc31b8
bhiller <benhiller@gmail.com>
2020-10-25 13:03:25 -0700

    always use postgres

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: process.env.PG_CONNECTION_STRING,
-  },
+  client: 'postgresql',
+  connection: process.env.PG_CONNECTION_STRING,
 };
