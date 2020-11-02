d6a1d0c82776b7a398f4c0069172fe4468f2eb34
bhiller <benhiller@gmail.com>
2020-11-02 12:04:39 -0800

    Revert "decrease max connections even more"
    This reverts commit e15f7113b82b4fd9896e29b26b1eda3f7a62988a.

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import ServerlessClient from 'serverless-postgres';
 
 const db = new ServerlessClient({
   connectionString: process.env.PG_CONNECTION_STRING,
-  maxConnections: 5,
+  maxConnections: 15,
 });
 // Use knex just as a query builder
 export const knex = require('knex')({ client: 'pg' });
