71f930bd4c5b14ef76baabe0402c73f73af87c80
bhiller <benhiller@gmail.com>
2020-11-02 20:40:54 -0800

    add google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -318,6 +318,20 @@ function HomePage({
         <meta property="og:url" content="https://sf.ballotanaltics.app/" />
         <meta property="og:image" content="/og.png" />
         <title>SF Ballot Analytics</title>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');`,
+          }}
+        />
       </Head>
       <style jsx>{styles}</style>
       <Header
